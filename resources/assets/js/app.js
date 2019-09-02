75a93a532e50060b2fd7cfc2d35a4b86ee0829bd
NikitaPilez <pileznikita99@gmail.com>
2019-09-02 15:13:58 +0300

    LaravelProject-6: Dynamics update line chart graphics.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -20,6 +20,7 @@ Vue.component('prop-component', require('./components/PropComponent.vue'));
 Vue.component('ajax-component', require('./components/AjaxComponent.vue'));
 Vue.component('chartline-component', require('./components/ChartlineComponent.vue'));
 Vue.component('chartpie-component', require('./components/ChartpieComponent.vue'));
+Vue.component('chartrandom-component', require('./components/ChartrandomComponent.vue'));
 
 const app = new Vue({
     el: '#app'
